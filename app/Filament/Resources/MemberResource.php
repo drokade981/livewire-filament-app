c0b3906a6358d1d7d998468667d341bf445310bc
drokade981@gmail.com <drokade981@gmail.comEngineer>
2024-09-08 22:36:42 +0530

    add members

---
+++ app/Filament/Resources/MemberResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MemberResource\Pages;
+use App\Filament\Resources\MemberResource\RelationManagers;
+use App\Models\Member;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use Nette\Utils\Image;
+use Filament\Tables\Columns\ImageColumn;
+
+class MemberResource extends Resource
+{
+    protected static ?string $model = Member::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->placeholder('Enter Name'),
+                TextInput::make('designation')->required()->placeholder('Enter Designation'),
+                TextInput::make('fb_url')->label('Facebook Url')->placeholder('Enter Facebook URL'),
+                TextInput::make('tw_url')->label('Twitter Url'),
+                TextInput::make('in_url')->label('Instagram Url'),
+                FileUpload::make('image'),
+                Select::make('status')->options(['active' => 'Active', 'inactive' => 'Inactive'])->default('active')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')->width(100),
+                TextColumn::make('name'),
+                TextColumn::make('designation'),
+                TextColumn::make('status'),
+                ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMembers::route('/'),
+            'create' => Pages\CreateMember::route('/create'),
+            'edit' => Pages\EditMember::route('/{record}/edit'),
+        ];
+    }
+}
