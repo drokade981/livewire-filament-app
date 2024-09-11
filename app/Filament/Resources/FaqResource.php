74f1d94d3aa0d3cb5f0a0fa881821947cda23818
drokade981@gmail.com <drokade981@gmail.comEngineer>
2024-09-12 01:10:36 +0530

    add faq and display

---
+++ app/Filament/Resources/FaqResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FaqResource\Pages;
+use App\Filament\Resources\FaqResource\RelationManagers;
+use App\Models\Faq;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+
+class FaqResource extends Resource
+{
+    protected static ?string $model = Faq::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('question'),
+                Select::make('status')->options(['active' => 'Active', 'inactive' => 'Inactive'])->default('active'),
+                RichEditor::make('answer')->columnspan(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('question'),
+                TextColumn::make('status'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFaqs::route('/'),
+            'create' => Pages\CreateFaq::route('/create'),
+            'edit' => Pages\EditFaq::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ resources/views/livewire/faq-page.blade.php
@@ -0,0 +1,36 @@
+<main>
+<section class="section">
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+      <div class="col-lg-6">
+        <div class="section-title text-center">
+          <p class="text-primary text-uppercase fw-bold mb-3">Frequient Questions</p>
+          <h1>Frequently Asked Questions</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-lg-9">
+        <div class="accordion accordion-border-bottom" id="accordionFAQ">
+        @forelse($faqs as $faq)  
+        <div class="accordion-item">
+            <h2 class="accordion-header accordion-button h5 border-0 @if($loop->first) active @endif"
+              id="heading-{{$loop->iteration}}" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapse-{{$loop->iteration}}" aria-expanded="@if($loop->first)true @else false @endif"
+              aria-controls="collapse-{{$loop->iteration}}">
+			  {{ $faq->question }}
+            </h2>
+            <div id="collapse-{{$loop->iteration}}"
+              class="accordion-collapse collapse border-0 show"
+              aria-labelledby="heading-{{$loop->iteration}}" data-bs-parent="#accordionFAQ">
+              <div class="accordion-body py-0 content">{!! $faq->answer !!}</div>
+            </div>
+          </div>
+        </div>
+        @empty
+        @endforelse
+      </div>
+    </div>
+  </div>
+</section>
+</main>
